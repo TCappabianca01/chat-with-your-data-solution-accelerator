abd83f4808f9af4b631fe9df1b90ffa763803138
Chris Westbrook <westbrook.chris@gmail.com>
2024-02-12 12:35:18 -0500

    fixed acr script

--- infra/acr/acr-cli-commands.sh
+++ infra/acr/acr-cli-commands.sh
@@ -1,17 +1,17 @@
 #Fill in these parameters before running this script
-rgName=resource-group-name
-location=location
-acrName=acr-name
+rgName=rg-testacrscript
+location=eastus
+acrName=cmwacrtestscript
 
 # Can use AZ CLI or Bicep to deploy the resources
 
 # Use Bicep to deploy the Azure Container Registry
-#az deployment group create --resource-group $rgName --template-file main.bicep --parameters location=$location acrName=$acrName
+az deployment group create --resource-group $rgName --template-file main.bicep --parameters location=$location acrName=$acrName
 
 # Use AZ CLI to create an Azure Container Registry
-az acr create --name $acrName --resource-group $rgName --sku Basic --admin-enabled false
+#az acr create --name $acrName --resource-group $rgName --sku Basic --admin-enabled false
 
 # Import a public image into the container registry
-az acr import --name $acrName --source fruoccopublic.azurecr.io --image rag-webapp:latest
-az acr import --name $acrName --source fruoccopublic.azurecr.io --image rag-adminwebapp:latest
-az acr import --name $acrName --source fruoccopublic.azurecr.io --image rag-backend:latest
+az acr import --resource-group $rgName --name $acrName --source fruoccopublic.azurecr.io/rag-webapp:latest --image rag-webapp:latest
+az acr import --resource-group $rgName --name $acrName --source fruoccopublic.azurecr.io/rag-adminwebapp:latest --image rag-adminwebapp:latest
+az acr import --resource-group $rgName --name $acrName --source fruoccopublic.azurecr.io/rag-backend:latest --image rag-backend:latest
